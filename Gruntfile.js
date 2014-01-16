db15c97f3957925a7636eab01c4c5a6e7d78343b
Nathan Hackley <nathanhackley@gmail.com>
2014-01-16 15:00:58 -0500

    automatically concat js / css with vendor folder, added fastclick

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,3 @@
-// Live Reload
 var liveReload = require('connect-livereload');
 var mountFolder = function(connect, dir) {
   return connect.static(require('path').resolve(dir));
@@ -7,29 +6,21 @@ var mountFolder = function(connect, dir) {
 
 module.exports = function(grunt) {
 
-	// Load all plugins named with the "grunt-" prefix
-	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+  // Load all plugins named with the "grunt-" prefix
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-	grunt.initConfig({
+  grunt.initConfig({
 
-    // Watch for file changes
-    // ----------------------------------------------------------------------------
-		watch: {
-      sass: { 
-        // When any scss file changes...
+    watch: {
+      sass: {
         files: ['_src/sass/**/*.scss'],
-        // ...run the sass:server task
-        tasks: ['sass:server']
+        tasks: ['sass:server', 'concat:server']
       },
       coffee: {
-        // When any coffee file changes...
         files: ['_src/coffee/**/*.coffee'],
-        // ...run the coffee:server task
-        tasks: ['coffee:server']
+        tasks: ['coffee:server', 'concat:server']
       },
       reload: {
-        // When any of these files change,
-        // refresh the browser.
         files: [
           '.tmp/css/*.css',
           '.tmp/js/*.js',
@@ -42,9 +33,7 @@ module.exports = function(grunt) {
       }
     },
 
-    // Set up our local server
-    // ----------------------------------------------------------------------------
-		connect: {
+    connect: {
       options: {
         port: 9000,
         hostname: '0.0.0.0'
@@ -62,94 +51,106 @@ module.exports = function(grunt) {
       },
     },
 
-    // Empty before a new build
-    // ----------------------------------------------------------------------------
-		clean: {
+    clean: {
       build: '_build',
-      server: '.tmp'
+      server: '.tmp',
     },
 
-    // Compile Sass, with different destinations for 'server' and 'build'
-    // ----------------------------------------------------------------------------
     sass: {
-  	 options: {
-        style: 'compressed',
+     options: {
+        style: 'expanded',
         precision: 15
       },
-	    build: { 
-	      files: {
-	        '_build/css/styles.css': '_src/sass/styles.scss'
-	      }
-	    },
-	    server: { 
-	      files: {
-	        '.tmp/css/styles.css': '_src/sass/styles.scss'
-	      }
-	    }
-	  },
+      build: { 
+        files: {
+          '.tmp/holding/css/styles.css': '_src/sass/styles.scss'
+        }
+      },
+      server: { 
+        files: {
+          '.tmp/holding/css/styles.css': '_src/sass/styles.scss'
+        }
+      }
+    },
 
-    // Compile CoffeeScript, with different destinations for 'server' and 'build'
-    // ----------------------------------------------------------------------------
-	  coffee: {
-	  	options: {
-	      join: true
-	    },
-	  	build: {
-		    files: {
-		      '_build/js/main.js': ['_src/coffee/**/*.coffee'] 
-		    }
-		  },
-		  server: {
-		    files: {
-		      '.tmp/js/main.js': ['_src/coffee/**/*.coffee'] 
-		    }
-		  }
-	  },
+    coffee: {
+      options: {
+        join: true
+      },
+      build: {
+        files: {
+          '.tmp/holding/js/main.js': ['_src/coffee/**/*.coffee'] 
+        }
+      },
+      server: {
+        files: {
+          '.tmp/holding/js/main.js': ['_src/coffee/**/*.coffee'] 
+        }
+      }
+    },
 
-    // Minify our newly compiled JS (build only)
-    // ----------------------------------------------------------------------------
-		uglify: {
-	    build: {
-	      files: {
-	        '_build/js/main.js': ['_build/js/main.js']
-	      }
-	    }
-	  },
+    concat: {
+      build: {
+        files: {
+          '_build/js/main.js': ['_src/vendor/**/*.js', '.tmp/holding/js/main.js'],
+          '_build/css/styles.css': ['_src/vendor/**/*.css', '.tmp/holding/css/styles.css'],
+        },
+      },
+      server: {
+        files: {
+          '.tmp/js/main.js': ['_src/vendor/**/*.js', '.tmp/holding/js/main.js'],
+          '.tmp/css/styles.css': ['_src/vendor/**/*.css', '.tmp/holding/css/styles.css'],
+        },
+      }
+    },
 
-    // Copy html, images, and other assets (build only)
-    // ----------------------------------------------------------------------------
-	  copy: {
-		  build: {
-		    files: [
-		      {expand: true, cwd: '_src/', src: ['index.html'], dest: '_build/'},
-		      {expand: true, cwd: '_src/', src: ['img/**'], dest: '_build/'},
-		      {expand: true, cwd: '_src/', src: ['vendor/**'], dest: '_build/'},
-		    ]
-		  }
-		}
+    uglify: {
+      build: {
+        files: {
+          '_build/js/main.js': ['_build/js/main.js']
+        }
+      }
+    },
+
+    cssmin: {
+      add_banner: {
+        files: {
+          '_build/css/styles.css': ['_build/css/styles.css']
+        }
+      }
+    },
+
+    copy: {
+      build: {
+        files: [
+          {expand: true, cwd: '_src/', src: ['*.html'], dest: '_build/'},
+          {expand: true, cwd: '_src/', src: ['img/**'], dest: '_build/'},
+        ]
+      }
+    }
   });
 
-  
-  // Register our Grunt tasks
-  // ------------------------------------------------------------------------------
 
-	grunt.registerTask('server', function(target) {
+  grunt.registerTask('server', function(target) {
     grunt.task.run([
       'clean:server',         // empty the ".tmp" directory
       'sass:server',          // compile sass
       'coffee:server',        // compile coffee
-      'connect',              // start up the server
+      'concat:server',        // bring in vendor js/css
+      'connect',              // start the server
       'watch'                 // watch for file changes
     ]);
   });
 
 
   grunt.registerTask('build', [
-  	'clean:build',							// empty the "build" directory
-    'sass:build', 							// compile sass
-    'coffee:build', 						// compile coffee
-    'uglify', 									// minify
-    'copy'											// copy all other files
+    'clean:build',              // empty the "build" directory
+    'sass:build',               // compile sass
+    'coffee:build',             // compile coffee
+    'concat:build',             // bring in vendor js/css
+    'uglify',                   // minify js
+    'cssmin',                   // minify css
+    'copy'                      // copy all other files
   ]);
 
   grunt.registerTask('default', 'build');
